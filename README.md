cfb30a49096c36edd78a070e9e0161aa35a44f20
Naman Khare <hi@namankhare.com>
2021-07-17 04:43:12 +0530

    Initial Push

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # control-video-remotely-using-nodejs-and-socket.io
+
+## Description
 Control Video Remotely using NodeJS and Socket.IO with an option to change background Color as well.
+
+## Example
+![Control video remotely Image](example/demo.png)
+![Control video remotely Gif](example/demogif.gif)
+
+## Installation 
+* Run ```npm install``` inside the terminal to install the node mmodules.
+* Change the video file inside the ```public``` folder.
+* Replace File path inside the index.js
+* ```npm start``` to run.
---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const router  = express.Router();
+const fs = require('fs');
+
+app.use(cors())
+app.use(router);
+app.set("view engine", "ejs");
+
+app.get("/", (req,res) => {
+    res.render('app')
+});
+
+router.get('/video', (req, res) => {
+    // video static path
+    const path = 'public/video.mp4';
+
+fs.stat(path, (err, stat) => {
+    //include video in nodejs
+    if (err !== null && err.code === 'ENOENT') {
+        res.sendStatus(404);
+    }
+
+    const fileSize = stat.size
+    const range = req.headers.range
+
+    if (range) {
+
+        const parts = range.replace(/bytes=/, "").split("-");
+
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1;
+        
+        const chunksize = (end-start)+1;
+        const file = fs.createReadStream(path, {start, end});
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        }
+        
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        }
+
+        res.writeHead(200, head);
+        fs.createReadStream(path).pipe(res);
+    }
+});
+});
+
+
+server.listen(3000, () => {
+    console.log("Server running at, http://localhost:3000")
+})
+
+io.on('connection', (socket) => {
+    console.log("User connected: "+ socket.id)
+
+    socket.on("message", (data) => {
+        socket.broadcast.emit('message', data)
+    })
+})
